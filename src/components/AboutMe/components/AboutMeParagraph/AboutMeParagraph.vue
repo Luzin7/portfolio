21f414a5552a433660f7b9f3947ca8ba72a9fee2
Luan Victor <lvictor.dutra@gmail.com>
2023-11-20 20:28:50 -0300

    feat: create paragraph about me

---
+++ src/components/AboutMe/components/AboutMeParagraph/AboutMeParagraph.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col items-center gap-4">
+    <h2 class="font-bold text-3xl md:text-4xl">Sobre mim</h2>
+    <p class="px-4 md:px-10 lg:mb-0">
+      Apaixonado por jogos, pelo filme Ratatouille e viciado em usar a gíria "tá
+      ligado", sou um desenvolvedor de software focado em automatizar processos
+      e entregar soluções eficientes.
+    </p>
+    <h2 class="font-bold text-3xl md:text-4xl">Por que programação?</h2>
+    <p class="mb-7 px-4 md:px-10 lg:mb-0">
+      Minha jornada na tecnologia começou na infância com interesse em segurança
+      de dados. Alimentando minha curiosidade por meio de projetos e
+      experimentos, explorei desde o desenvolvimento de jogos digitais até
+      mobile.
+    </p>
+  </div>
+</template>
