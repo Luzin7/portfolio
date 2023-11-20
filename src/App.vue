4b124c537a59b55ecca20940a358385c937747b8
Luan Victor <lvictor.dutra@gmail.com>
2023-11-20 20:28:02 -0300

    feat: add aboutme componente

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
+import AboutMe from './components/AboutMe/AboutMe.vue';
 import HeaderContainer from './components/Header/HeaderContainer.vue';
 </script>
 
 <template>
   <HeaderContainer />
+  <main>
+    <AboutMe />
+  </main>
 </template>
