2cb048dcbcfc819a8115c9456647a83067ef72cc
Luan Victor <lvictor.dutra@gmail.com>
2023-11-20 20:28:21 -0300

    feat: create compontent about me

---
+++ src/components/AboutMe/AboutMe.vue
@@ -0,0 +1,17 @@
+<template>
+  <section
+    class="bg-backgroundDefault min-h-[93vh] flex flex-col items-center justify-center"
+  >
+    <div class="container">
+      <div class="grid grid-cols-1 lg:grid-cols-2">
+        <AboutMeParagraph />
+        <MyCoreSkills />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import AboutMeParagraph from './components/AboutMeParagraph/AboutMeParagraph.vue';
+import MyCoreSkills from './components/MyCoreSkills/MyCoreSkills.vue';
+</script>
