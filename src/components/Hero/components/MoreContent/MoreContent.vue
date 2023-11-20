4d6e4038e448cbe9827b03c5b2439082bf0cbd72
Luan Victor <lvictor.dutra@gmail.com>
2023-11-19 22:19:46 -0300

    feat: create button to hero

---
+++ src/components/Hero/components/MoreContent/MoreContent.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="mx-auto mt-5">
+    <button class="bg-accent py-3.5 px-9 rounded-md font-bold">
+      Entre em contato
+    </button>
+  </div>
+</template>
+
+<script setup></script>
