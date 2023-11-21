b0f6bdc029b9986238bb87aa9cc2a522829dc0ea
Luan Victor <lvictor.dutra@gmail.com>
2023-11-21 15:59:42 -0300

    update: readjust paragraph

--- src/components/AboutMe/components/AboutMeParagraph/AboutMeParagraph.vue
+++ src/components/AboutMe/components/AboutMeParagraph/AboutMeParagraph.vue
@@ -2,9 +2,9 @@
   <div class="flex flex-col items-center gap-4">
     <h2 class="font-bold text-3xl md:text-4xl">Sobre mim</h2>
     <p class="px-4 md:px-10 lg:mb-0">
-      Apaixonado por jogos, pelo filme Ratatouille e viciado em usar a gíria "tá
-      ligado", sou um desenvolvedor de software focado em automatizar processos
-      e entregar soluções eficientes.
+      Apaixonado por jogos, meu pernambuco, pelo filme Ratatouille e viciado em
+      usar a gíria "tá ligado", sou um desenvolvedor de software focado em
+      automatizar processos e entregar soluções eficientes.
     </p>
     <h2 class="font-bold text-3xl md:text-4xl">Por que programação?</h2>
     <p class="mb-7 px-4 md:px-10 lg:mb-0">
