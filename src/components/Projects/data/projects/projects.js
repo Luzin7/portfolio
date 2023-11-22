027492cbe915e3bca1addbff7325b3df760ab97c
Luan Victor <lvictor.dutra@gmail.com>
2023-11-22 17:48:55 -0300

    feat: create a list of projects

---
+++ src/components/Projects/data/projects/projects.js
@@ -0,0 +1,34 @@
+import devorumImg from '../../../../assets/images/projects/devorum/devorum.png';
+import cenaestelarImg from '../../../../assets/images/projects/cenaestelar/cenaestelar.png';
+import skycastImg from '../../../../assets/images/projects/skycast/skycast.png';
+
+export const projects = [
+  {
+    media: devorumImg,
+    name: 'Devorum',
+    link: 'https://devorum.vercel.app/',
+    description: `Uma plataforma de perguntas e respostas para atender às necessidades de
+    quem precisa. A plataforma permite que façam perguntas, obtenham ajuda
+    e interajam em um ambiente de aprendizado colaborativo.`,
+  },
+  {
+    media: cenaestelarImg,
+    name: 'Cena Estelar',
+    link: 'https://cenaestelar.netlify.app/de',
+    description: `Um projeto em desenvolvimento de filmes que assisti com meus
+     amigos. Junto aos títulos, adicionamos nossas opiniões, média de notas e disponibilzamos 
+     uma seção de filmes que pretendemos assistir.`,
+  },
+  {
+    media: skycastImg,
+    name: 'SkyCast',
+    link: 'https://luzin7.github.io/skycast/',
+    description: `Uma aplicação web para visualização de informações climáticas em tempo real. `,
+  },
+  {
+    media: null,
+    name: 'Em breve',
+    link: null,
+    description: 'Estou trabalhando em um produto ainda melhor!',
+  },
+];
