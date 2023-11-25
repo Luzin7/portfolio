f05c4c9598e166edce1cf82945674829cae5d992
Luan Victor <lvictor.dutra@gmail.com>
2023-11-25 16:26:42 -0300

    feat: add function to button

--- src/components/Hero/components/MoreContent/MoreContent.vue
+++ src/components/Hero/components/MoreContent/MoreContent.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="mx-auto mt-5">
-    <button class="bg-accent py-3.5 px-9 rounded-md font-bold">
+    <button
+      type="button"
+      class="bg-accent py-3.5 px-9 rounded-md font-bold"
+      @click="() => scrollToSection('contact')"
+    >
       Entre em contato
     </button>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import scrollToSection from '../../../../utils/scrollToSection';
+</script>
