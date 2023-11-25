dab2ba99ee785301958450a1313ccabcf8470b2d
Luan Victor <lvictor.dutra@gmail.com>
2023-11-25 15:39:27 -0300

    feat: create and export utils

---
+++ src/utils/socials.js
@@ -0,0 +1,3 @@
+export const linkedin = 'https://www.linkedin.com/in/lvictordutra/';
+export const github = 'https://github.com/Luzin7';
+export const portfolio = 'https://lvictor-portfolio.vercel.app/';
