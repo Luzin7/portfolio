1d2ae14fe5a2e9881845741fabc2a2df60d4c205
Luan Victor <lvictor.dutra@gmail.com>
2023-11-26 22:53:48 -0300

    perf: transform components to async

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <script setup>
-import AboutMe from './components/AboutMe/AboutMe.vue';
-import HeaderContainer from './components/Header/HeaderContainer.vue';
-import FooterContainer from './components/Footer/FooterContainer.vue';
-import HeroContainer from './components/Hero/HeroContainer.vue';
-import MyProjects from './components/Projects/MyProjects.vue';
-import ContactForm from './components/ContactForm/ContactForm.vue';
+import { defineAsyncComponent } from 'vue';
+
+const AboutMe = defineAsyncComponent(
+  () => import('./components/AboutMe/AboutMe.vue'),
+);
+const HeaderContainer = defineAsyncComponent(
+  () => import('./components/Header/HeaderContainer.vue'),
+);
+const FooterContainer = defineAsyncComponent(
+  () => import('./components/Footer/FooterContainer.vue'),
+);
+const HeroContainer = defineAsyncComponent(
+  () => import('./components/Hero/HeroContainer.vue'),
+);
+const MyProjects = defineAsyncComponent(
+  () => import('./components/Projects/MyProjects.vue'),
+);
+const ContactForm = defineAsyncComponent(
+  () => import('./components/ContactForm/ContactForm.vue'),
+);
 </script>
 
 <template>
