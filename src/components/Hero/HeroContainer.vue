61ff330ca5117e6bdc358e82551c34035f7ca71b
Luan Victor <lvictor.dutra@gmail.com>
2023-11-26 22:59:42 -0300

    ref: transform all content to component

--- src/components/Hero/HeroContainer.vue
+++ src/components/Hero/HeroContainer.vue
@@ -3,30 +3,14 @@
     id="home"
     class="bg-backgroundSecondary min-h-screen mx-auto flex flex-col justify-center"
   >
-    <div class="flex flex-col items-center justify-center">
-      <h1 class="text-3xl md:text-5xl font-bold text-white text-center">
-        Olá, me chamo Luan Victor! <br />
-        Desenvolvedor
-        <span class="underline decoration-primary">Fullstack</span>
-      </h1>
-      <p
-        class="opacity-80 text-base w-9/12 lg:w-5/12 2xl:w-3/12 lg:text-base text-center mx-auto pt-2 lg:pt-4"
-      >
-        Sou um desenvolvedor de software focado em automatizar processos e
-        entregar soluções eficientes.
-      </p>
-    </div>
-    <MoreContent />
-    <div class="absolute bottom-10 left-1/2 transform -translate-x-1/2">
-      <Icon
-        icon="material-symbols-light:expand-more-rounded"
-        class="animate-bounce text-4xl"
-      />
-    </div>
+    <PresentationComponent />
+    <ContactButton />
+    <ScrollDownIcon />
   </section>
 </template>
 
 <script setup>
-import { Icon } from '@iconify/vue';
-import MoreContent from './components/MoreContent/MoreContent.vue';
+import ContactButton from './components/ContactButton/ContactButton.vue';
+import PresentationComponent from './components/Presentation/PresentationComponent.vue';
+import ScrollDownIcon from './components/ScrollDownIcon/ScrollDownIcon.vue';
 </script>
