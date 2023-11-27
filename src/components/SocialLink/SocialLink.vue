071606ecf171f46a69b1a9044312dd7ed67a63df
Luan Victor <lvictor.dutra@gmail.com>
2023-11-26 23:02:39 -0300

    feat: create social link component

---
+++ src/components/SocialLink/SocialLink.vue
@@ -0,0 +1,27 @@
+<template>
+  <li>
+    <a
+      :href="href"
+      target="_blank"
+      rel="noopener noreferrer"
+      class="hover:text-accent transition-colors"
+      :aria-label="`Acesse
+      meu
+      perfil
+      no
+      ${socialName}`"
+    >
+      <Icon :icon="icon" class="text-4xl xl:text-3xl" />
+    </a>
+  </li>
+</template>
+
+<script setup>
+import { Icon } from '@iconify/vue';
+
+defineProps({
+  href: String,
+  icon: String,
+  socialName: String,
+});
+</script>
