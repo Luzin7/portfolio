1f339048b6de693ff306cabd4e41177a9fcac9ca
Luan Victor <lvictor.dutra@gmail.com>
2023-11-26 23:00:39 -0300

    feat: create description component of presentation

---
+++ src/components/Hero/components/Presentation/PresentationDescription/PresentationDescription.vue
@@ -0,0 +1,8 @@
+<template>
+  <p
+    class="opacity-80 text-base w-9/12 lg:w-5/12 2xl:w-3/12 lg:text-base text-center mx-auto pt-2 lg:pt-4"
+  >
+    Sou um desenvolvedor de software focado em automatizar processos e entregar
+    soluções eficientes.
+  </p>
+</template>
