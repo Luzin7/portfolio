43a38596690cfb4b735067d83a2070c01803ffa7
Luan Victor <lvictor.dutra@gmail.com>
2023-11-26 22:58:13 -0300

    feat: create navlink
    create navlink template to use in header

---
+++ src/components/Header/components/NavigationLinks/components/NavLink.vue
@@ -0,0 +1,18 @@
+<template>
+  <li
+    class="hover:text-accent transition-colors cursor-pointer"
+    :aria-label="`Vá para a seção ${text}`"
+    @click="() => scrollToSection(sectionToScroll)"
+  >
+    {{ text }}
+  </li>
+</template>
+
+<script setup>
+import scrollToSection from '../../../../../utils/scrollToSection';
+
+defineProps({
+  sectionToScroll: String,
+  text: String,
+});
+</script>
