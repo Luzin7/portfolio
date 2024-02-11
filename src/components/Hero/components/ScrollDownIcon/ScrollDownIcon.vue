13accec018684053d82fab08778e33d58bb69801
Luzin7 <lvictor.dutra@gmail.com>
2024-02-11 12:31:33 -0300

    style: add opacity by hover

--- src/components/Hero/components/ScrollDownIcon/ScrollDownIcon.vue
+++ src/components/Hero/components/ScrollDownIcon/ScrollDownIcon.vue
@@ -2,7 +2,7 @@
   <div class="absolute bottom-10 left-1/2 transform -translate-x-1/2">
     <Icon
       icon="material-symbols-light:expand-more-rounded"
-      class="animate-bounce text-4xl"
+      class="animate-bounce text-4xl hover:opacity-40 transition-opacity"
     />
   </div>
 </template>
