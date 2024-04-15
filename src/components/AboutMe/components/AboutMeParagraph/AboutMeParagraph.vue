59fb301a0d0a780464d9b01a934686be3864df4d
Luzin7 <lvictor.dutra@gmail.com>
2024-04-14 23:39:31 -0300

    update: add more about me and motivations

--- src/components/AboutMe/components/AboutMeParagraph/AboutMeParagraph.vue
+++ src/components/AboutMe/components/AboutMeParagraph/AboutMeParagraph.vue
@@ -3,15 +3,17 @@
     <h2 class="font-bold text-3xl md:text-4xl">Sobre mim</h2>
     <p class="px-4 md:px-10 mb-7">
       Apaixonado por jogos, meu pernambuco, pelo filme Ratatouille e viciado em
-      usar a gíria "tá ligado", sou um desenvolvedor de software focado em
-      automatizar processos e entregar soluções eficientes.
+      usar a gíria "tá ligado", sou um <strong>desenvolvedor de software</strong> que estuda
+      <strong>Engenharia de Software</strong> e gosto de <strong>automatizar processos</strong> e entregar <strong>soluções eficientes</strong>.
+      Costumo <strong>ajudar</strong> e ser <strong>proativo</strong> nas <strong>comunidades</strong> que estou presente, alem de desenvolver ferramentas com o mesmo propósito.
+      <i>Se eu souber eu vou te ajudar, se não, pesquisamos e aprendemos juntos.</i> Tenho conhecimentos sólidos no <strong>frontend</strong> e <strong>backend</strong>, mas estou sempre em busca de evolução.
     </p>
     <h2 class="font-bold text-3xl md:text-4xl">Por que programação?</h2>
     <p class="mb-7 px-4 md:px-10 lg:mb-0">
       Minha jornada na tecnologia começou na infância com interesse em segurança
       de dados. Alimentando minha curiosidade por meio de projetos e
-      experimentos, explorei desde o desenvolvimento de jogos digitais até
-      mobile.
+      experimentos, explorei desde o <strong>desenvolvimento de jogos digitais até
+      mobile</strong>. Hoje <strong>me sinto confortável desenvolvendo coisas que fazem sentido e que resolvam problemas</strong>.
     </p>
   </div>
 </template>
