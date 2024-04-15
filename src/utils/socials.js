2327a1c5fe98813633b9c290a37f74b3bf86d812
Luzin7 <lvictor.dutra@gmail.com>
2024-04-14 23:38:22 -0300

    feat: add youtube channel

--- src/utils/socials.js
+++ src/utils/socials.js
@@ -1,3 +1,5 @@
 export const linkedin = 'https://www.linkedin.com/in/lvictordutra/';
 export const github = 'https://github.com/Luzin7';
 export const portfolio = 'https://lvictor-portfolio.vercel.app/';
+export const youtube = 'https://www.youtube.com/@devluanvictor';
+
