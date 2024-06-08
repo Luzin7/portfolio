47c864a58bb186a12d016c57bf9c88d83301b0cc
Luzin7 <lvictor.dutra@gmail.com>
2024-06-08 17:05:25 -0300

    ref: change about me section

--- src/components/AboutMe/components/AboutMeParagraph/AboutMeParagraph.vue
+++ src/components/AboutMe/components/AboutMeParagraph/AboutMeParagraph.vue
@@ -1,19 +1,61 @@
 <template>
-  <div class="flex flex-col items-center gap-4">
-    <h2 class="font-bold text-3xl md:text-4xl">Sobre mim</h2>
-    <p class="px-4 md:px-10 mb-7">
-      Apaixonado por jogos, meu pernambuco, pelo filme Ratatouille e viciado em
-      usar a gíria "tá ligado", sou um <strong>desenvolvedor de software</strong> que estuda
-      <strong>Engenharia de Software</strong> e gosto de <strong>automatizar processos</strong> e entregar <strong>soluções eficientes</strong>.
-      Costumo <strong>ajudar</strong> e ser <strong>proativo</strong> nas <strong>comunidades</strong> que estou presente, alem de desenvolver ferramentas com o mesmo propósito.
-      <i>Se eu souber eu vou te ajudar, se não, pesquisamos e aprendemos juntos.</i> Tenho conhecimentos sólidos no <strong>frontend</strong> e <strong>backend</strong>, mas estou sempre em busca de evolução.
-    </p>
-    <h2 class="font-bold text-3xl md:text-4xl">Por que programação?</h2>
-    <p class="mb-7 px-4 md:px-10 lg:mb-0">
-      Minha jornada na tecnologia começou na infância com interesse em segurança
-      de dados. Alimentando minha curiosidade por meio de projetos e
-      experimentos, explorei desde o <strong>desenvolvimento de jogos digitais até
-      mobile</strong>. Hoje <strong>me sinto confortável desenvolvendo coisas que fazem sentido e que resolvam problemas</strong>.
-    </p>
+  <h2 class="font-bold text-4xl text-center mb-8 md:mb-0 max-w-[90%]">
+    Por Trás do Código!
+  </h2>
+  <div class="flex flex-col items-center gap-8 max-w-[90%] lg:max-w-[80%] xl:max-w-[75%] md:flex-row">
+    <div>
+      <p class="text-textSecondary leading-7">
+        Apaixonado por jogos, meu pernambuco, pelo filme Ratatouille e viciado
+        em usar a gíria "tá ligado", sou um
+        <strong>desenvolvedor de software</strong> que cursa
+        <strong>Engenharia de Software</strong>. Gosto de
+        <strong>automatizar processos</strong> e entregar
+        <strong>soluções eficientes</strong>.
+        <br />
+        Costumo <strong>ajudar</strong> e ser <strong>proativo</strong> nas
+        <strong>comunidades</strong> que estou presente, alem de desenvolver
+        ferramentas com o mesmo propósito.
+        <i
+          >Se eu souber eu vou te ajudar, se não, pesquisamos e aprendemos
+          juntos.</i
+        >
+      </p>
+    </div>
+
+    <div class="flex items-center justify-between md:flex-col gap-8">
+      <div class="flex flex-col justify-center items-center">
+        <span
+          class="text-3xl font-bold text-primaryForeground md:text-5xl"
+          ><span>4</span>+</span
+        >
+        <h2
+          class="text-sm font-medium capitalize text-center text-textSecondary xl:text-base"
+        >
+          Clientes satisfeitos
+        </h2>
+      </div>
+      <div class="flex flex-col justify-center items-center">
+        <span
+          class="text-3xl font-bold text-primaryForeground md:text-5xl"
+          ><span>7</span>+</span
+        >
+        <h2
+          class="text-sm font-medium capitalize text-center text-textSecondary xl:text-base"
+        >
+          Projetos completo
+        </h2>
+      </div>
+      <div class="flex flex-col justify-center items-center">
+        <span
+          class="text-3xl font-bold text-primaryForeground md:text-5xl"
+          ><span>2</span>+</span
+        >
+        <h2
+          class="text-sm font-medium capitalize text-center text-textSecondary xl:text-base"
+        >
+          Anos de experiência
+        </h2>
+      </div>
+    </div>
   </div>
 </template>
