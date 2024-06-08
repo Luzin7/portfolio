7a61a5b09296137c8f9aecdc2734d70fc3d6fcb3
Luzin7 <lvictor.dutra@gmail.com>
2024-06-08 17:05:53 -0300

    feat: create available to work component

---
+++ src/components/AvailableToWork/AvailableToWork.vue
@@ -0,0 +1,14 @@
+<template>
+ <div class="flex flex-row gap-2 items-center">
+      <span class="relative flex h-2 w-2"
+        ><span
+          class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"
+        ></span
+        ><span
+          class="relative inline-flex rounded-full h-2 w-2 bg-green-500"
+        ></span></span
+      ><span class="font-satoshi-semibold font-medium text-base text-green-500"
+        >Disponível para Trabalho</span
+      >
+    </div>
+</template>
