68d470e54bbf8b04191f502b0645945b263ae7e2
Luzin7 <lvictor.dutra@gmail.com>
2024-06-08 17:06:41 -0300

    ref: change text

--- src/components/Hero/components/Presentation/PresentationDescription/PresentationDescription.vue
+++ src/components/Hero/components/Presentation/PresentationDescription/PresentationDescription.vue
@@ -1,8 +1,7 @@
 <template>
   <p
-    class="opacity-80 text-base w-9/12 lg:w-5/12 2xl:w-3/12 lg:text-base text-center mx-auto pt-2 lg:pt-4"
+    class="text-textSecondary text-base lg:text-lg text-center"
   >
-    Apaixonado por automatizar processos repetitivos e criar workflows
-    otimizados que impulsionam a produtividade e o desempenho.
+  Desenvolvedor de Software com foco em criar soluções eficientes e de alta performance.
   </p>
 </template>
--- src/components/Hero/components/Presentation/PresentationTitle/PresentationTitle.vue
+++ src/components/Hero/components/Presentation/PresentationTitle/PresentationTitle.vue
@@ -1,6 +1,6 @@
 <template>
-  <h1 class="text-3xl md:text-5xl font-bold text-white text-center">
-    Olá, sou Luan Victor! <br />
-    Desenvolvedor de Software
+  <h1 class="text-4xl md:text-5xl xl:text-6xl font-bold text-white text-center">
+    Sou Desenvolvedor de Software <br />
+    <span class="text-2xl xl:text-4xl">e me chamo Luan Victor!</span>
   </h1>
 </template>
