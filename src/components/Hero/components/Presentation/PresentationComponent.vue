9aedcfb5adaa2ba6c405c7c561531b873a03a77a
Luzin7 <lvictor.dutra@gmail.com>
2024-06-08 17:06:59 -0300

    feat: add new component

--- src/components/Hero/components/Presentation/PresentationComponent.vue
+++ src/components/Hero/components/Presentation/PresentationComponent.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="flex flex-col items-center justify-center">
+  <div class="flex flex-col items-center justify-center gap-8">
+    <AvailableToWork />
     <PresentationTitle />
     <PresentationDescription />
   </div>
 </template>
 
 <script setup>
-import PresentationTitle from './PresentationTitle/PresentationTitle.vue';
-import PresentationDescription from './PresentationDescription/PresentationDescription.vue';
+import AvailableToWork from "../../../AvailableToWork/AvailableToWork.vue";
+import PresentationDescription from "./PresentationDescription/PresentationDescription.vue";
+import PresentationTitle from "./PresentationTitle/PresentationTitle.vue";
 </script>
