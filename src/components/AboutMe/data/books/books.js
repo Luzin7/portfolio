c828ac8373007ce558549fa8f419f8dda9b5a6f1
Luzin7 <lvictor.dutra@gmail.com>
2024-06-21 20:10:02 -0300

    refactor:  update objects

---
+++ src/components/AboutMe/data/books/books.js
@@ -0,0 +1,19 @@
+const recommendedBooks = [
+  {
+    name: 'Cartas de um Estoico',
+    image: 'https://m.media-amazon.com/images/I/81fzZ5aFTUL._SL1500_.jpg',
+    url: 'https://www.amazon.com.br/Cartas-um-Estoico-I-ebook/dp/B07899SZBN/ref=sr_1_1?crid=NY4KJNQMESRB&dib=eyJ2IjoiMSJ9.gJf_XFWHZOn-prfohV9tM9GfRsHYLnVNmt1Z_AtuZwOQuNurAshcukkM35mWJUiWZP13s7QGMqOkPC9MYGa4UGAHtTP1pX86XJ8_u98Px6Bu5WNHgJg-uRzcrEvGAQWwbhxUVu57121HUa_sJLnGvTZMZOUoHheUfVj6VXB2N_dYRvWQ8Etl0or5l_xNfbTJMm_emhIQKa-a-6PRtD-L0_NzqZvdghbji7AuB9R8eZM.YftbqBAqiaP73vcTOlpqq5PHTmYL6ls0c2PyOa2YCno&dib_tag=se&keywords=cartas+de+um+estoico&qid=1719002879&s=digital-text&sprefix=cartasde+%2Cdigital-text%2C171&sr=1-1'
+  },
+  {
+    name: 'Clean Code',
+    image: 'https://www.altabooks.com.br/wp-content/uploads/2019/07/Codigo_Limpo.jpg',
+    url: 'https://www.amazon.com.br/s?k=clean+code&i=stripbooks&crid=1EHQSZLM9JFUD&sprefix=Cllea%2Cstripbooks%2C172&ref=nb_sb_ss_sc_1_4'
+  },
+  {
+    name: '14 Hábitos de Desenvolvedores Altamente Produtivos',
+    image: 'https://m.media-amazon.com/images/I/71QkHuZFf8L._SY466_.jpg',
+    url: 'https://www.amazon.com.br/14-H%C3%A1bitos-Desenvolvedores-Altamente-Produtivos-ebook/dp/B08BF7PZZX/ref=sr_1_1?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=368LYFFE1R2FY&dib=eyJ2IjoiMSJ9.L0vesxbko0SshpU0kXXjzj9vrDpwb9673lJINsR-QC6mGGf4y87NwP0PbpbmQAgc3SdajchETyUkVw1poOSA0SuLMXiMNdEy4tzS6ri40DaHA4QRLjB11wrZH3yCqz89diPdvjv72INL_1pr4Ntt6g.yOi2rd8oZc8hEFPAW55BpibFIlbXT6s4tHEWZomEsrI&dib_tag=se&keywords=14+H%C3%A1bitos+de+Desenvolvedores+Altamente+Produtivos&qid=1719004064&s=digital-text&sprefix=14+h%C3%A1bitos+de+desenvolvedores+altamente+produtivos%2Cdigital-text%2C159&sr=1-1'
+  },
+];
+
+export default recommendedBooks;
