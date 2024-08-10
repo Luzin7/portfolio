15fcc388db2fb138cc501b8f6705582d640ff74b
Luzin7 <lvictor.dutra@gmail.com>
2024-08-10 01:08:04 -0300

    refactor: adjust components order

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,21 @@
 <script setup>
 import { defineAsyncComponent } from "vue";
 import AboutMe from "./components/AboutMe/AboutMe.vue";
-// import HeaderContainer from './components/Header/HeaderContainer.vue';
 import HeroContainer from "./components/Hero/HeroContainer.vue";
 const FooterContainer = defineAsyncComponent(
   () => import("./components/Footer/FooterContainer.vue"),
 );
-// const MyProjects = defineAsyncComponent(
-//   () => import('./components/Projects/MyProjects.vue'),
-// );
-// const ContactForm = defineAsyncComponent(
-//   () => import('./components/ContactForm/ContactForm.vue'),
-// );
+const MyProjects = defineAsyncComponent(
+  () => import("./components/Projects/MyProjects.vue"),
+);
 </script>
 
 <template>
-  <!-- <HeaderContainer /> -->
   <main class="container mx-auto">
     <HeroContainer />
-    <AboutMe />
+    <MyProjects />
 
-    <!-- <MyProjects />  -->
-    <!-- <ContactForm /> -->
+    <AboutMe />
   </main>
   <FooterContainer class="max-sm:pb-12" />
 </template>
